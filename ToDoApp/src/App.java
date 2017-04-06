f595888942039bc258496a01004ab7cc0d137cf9
Patrik <horvath.patrik001@gmail.com>
2017-04-06 12:28:50 +0200

    ToDoApp - read the txt file, print it out if args == 0

--- ToDoApp/src/App.java
+++ ToDoApp/src/App.java
@@ -1,9 +1,22 @@
-import java.util.Arrays;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class App {
 
   public static void main(String[] args) {
-    System.out.println(Arrays.toString(args));
+    Path text = Paths.get("printusage.txt");
+    if (args.length == 0) {
+      try {
+        List<String> noArgument = Files.readAllLines(text);
+        for (int i = 0; i < noArgument.size(); i++) {
+          System.out.println(noArgument.get(i));
+        }
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
   }
-
 }
