b1c01379fe111035b24da01c1b7f9dd108e78b9a
Patrik <horvath.patrik001@gmail.com>
2017-04-06 14:28:33 +0200

    ToDoApp - Create the ListTask list

--- ToDoApp/src/App.java
+++ ToDoApp/src/App.java
@@ -1,22 +1,17 @@
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
+
+import java.util.Arrays;
 
 public class App {
 
   public static void main(String[] args) {
-    Path text = Paths.get("printusage.txt");
+
+    ToDoList toDoList;
+    toDoList = new ToDoList();
+
     if (args.length == 0) {
-      try {
-        List<String> noArgument = Files.readAllLines(text);
-        for (int i = 0; i < noArgument.size(); i++) {
-          System.out.println(noArgument.get(i));
-        }
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
+      toDoList.NoArgument();
+    } else if (Arrays.toString(args).contains("-l")) {
+      toDoList.ListTask();
     }
   }
 }
--- ToDoApp/src/ToDoList.java
+++ ToDoApp/src/ToDoList.java
@@ -1,3 +1,37 @@
-public class ToDoList {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
+public class ToDoList {
+  Path listtasksText;
+  Path printusageText;
+
+  public void ListTask() {
+    try {
+      listtasksText = Paths.get("listtasks.txt");
+      List<String> orderedList = Files.readAllLines(listtasksText);
+      for (int i = 0; i < orderedList.size(); i++) {
+        System.out.println(i + 1 + " " + orderedList.get(i));
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+      System.out.println("Uh-oh, could not read the file!");
+    }
+  }
+
+  public void NoArgument() {
+    try {
+      printusageText = Paths.get("printusage.txt");
+      List<String> noArgument = Files.readAllLines(printusageText);
+      for (int i = 0; i < noArgument.size(); i++) {
+        System.out.println(noArgument.get(i));
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+      System.out.println("Uh-oh, could not read the file!");
+    }
+  }
 }
+
