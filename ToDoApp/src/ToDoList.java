867942f4ed02369983c7f90af490d99a991dc8af
Patrik <horvath.patrik001@gmail.com>
2017-04-07 11:24:05 +0200

    Simplifying code

--- ToDoApp/src/ToDoList.java
+++ ToDoApp/src/ToDoList.java
@@ -8,6 +8,13 @@ import java.util.List;
 public class ToDoList {
   Path listtasksText;
   Path printusageText;
+  List<String> list;
+  String errorNoTask = "System error: No task is provided!";
+  String errorNoIndex = "System error: No index is provided!";
+  String errorOutIndex = "System error: Index is out of list size!";
+  String errorNotNumber = "System error: Index is not a number!";
+  String updatedList = "Here is your new list:";
+  String taskFile = "listtasks.txt";
 
   public void NoArgument() {
     try {
@@ -23,7 +30,7 @@ public class ToDoList {
 
   public void ListTask() {
     try {
-      listtasksText = Paths.get("listtasks.txt");
+      listtasksText = Paths.get(taskFile);
       List<String> orderedList = Files.readAllLines(listtasksText);
       if (orderedList.size() == 0) {
         System.out.println("nothing to do today! :)");
@@ -31,7 +38,6 @@ public class ToDoList {
         for (int i = 0; i < orderedList.size(); i++) {
           if (i < 9) {
             System.out.println(" " + (i + 1) + " - " + orderedList.get(i));
-
           } else {
             System.out.println(i + 1 + " - " + orderedList.get(i));
           }
@@ -43,57 +49,62 @@ public class ToDoList {
   }
 
   public void AddTask(String[] args) {
-    Path tasks = Paths.get("listtasks.txt");
-    List<String> list;
+    listtasksText = Paths.get(taskFile);
     try {
-      list = Files.readAllLines(tasks);
+      list = Files.readAllLines(listtasksText);
       if (args.length == 1) {
-        System.out.println("System error: No task is provided!");
+        System.out.println(errorNoTask);
       } else {
         list.add(list.size(), "[ ]" + args[1]);
-        Files.write(tasks, list);
+        Files.write(listtasksText, list);
       }
+      System.out.println("Here is your new list:");
+      ListTask();
+      System.out.println();
     } catch (Exception e) {
-      e.printStackTrace();
     }
   }
 
   public void RemoveTask(String[] args) {
-    Path tasks = Paths.get("listtasks.txt");
-    List<String> list;
+    listtasksText = Paths.get(taskFile);
     try {
-      list = Files.readAllLines(tasks);
+      list = Files.readAllLines(listtasksText);
       if (args.length == 1) {
-        System.out.println("System error: No index is provided!");
+        System.out.println(errorNoIndex);
       } else if (Integer.parseInt(args[1]) > list.size()) {
-        System.out.println("System error: Index is out of list size!");
+        System.out.println(errorOutIndex);
       } else {
         list.remove(Integer.parseInt(args[1]) - 1);
-        Files.write(tasks, list);
+        Files.write(listtasksText, list);
       }
+      System.out.println(updatedList);
+      ListTask();
+      System.out.println();
     } catch (Exception e) {
-      System.out.println("System error: Index is not a number!");
+      System.out.println(errorNotNumber);
     }
   }
 
   public void CheckTask(String[] args) {
-    Path tasks = Paths.get("listtasks.txt");
-    List<String> list;
+    listtasksText = Paths.get(taskFile);
     String chosenTask;
     try {
-      list = Files.readAllLines(tasks);
+      list = Files.readAllLines(listtasksText);
       if (args.length == 1) {
-        System.out.println("System error: No index is provided!");
+        System.out.println(errorNoIndex);
       } else if (Integer.parseInt(args[1]) > list.size()) {
-        System.out.println("System error: Index is out of list size!");
+        System.out.println(errorOutIndex);
       } else {
         chosenTask = list.get(Integer.parseInt(args[1]) - 1);
         String newString = chosenTask.replaceFirst(" ", "x");
         list.set((Integer.parseInt(args[1]) - 1), newString);
-        Files.write(tasks, list);
+        Files.write(listtasksText, list);
       }
+      System.out.println(updatedList);
+      ListTask();
+      System.out.println();
     } catch (Exception e) {
-      e.printStackTrace();
+      System.out.println(errorNotNumber);
     }
   }
 }
